06d6e8f6545774b3682b89978ab7684e115c05bd
jmgirard <me@jmgirard.com>
2024-12-30 12:43:11 -0500

    add expand_optional tests

--- tests/testthat/test-expand.R
+++ tests/testthat/test-expand.R
@@ -1,12 +1,30 @@
-test_that("expansion works with separate sets", {
+# test expand_optional ---------------------------------------------------------
+testthat::test_that("expansion works with separate sets", {
+  # Stringify works
   t1 <- expand_optional(
     require = c(1, 3),
     optional = 2,
     stringify = TRUE
   )
+  testthat::expect_type(t1, "character")
+  testthat::expect_length(t1, 2)
+  testthat::expect_equal(t1[[1]], "1+3")
+  testthat::expect_equal(t1[[2]], "1+2+3")
+
+  # Non-stringify works
+  t2 <- expand_optional(
+    require = c(1, 3),
+    optional = 2,
+    stringify = FALSE
+  )
+  testthat::expect_type(t2, "list")
+  testthat::expect_length(t2, 2)
+  testthat::expect_equal(t2[[1]], c(1, 3))
+  testthat::expect_equal(t2[[2]], c(1, 2, 3))
 })
 
-test_that("subsets works", {
+# test get_subsets -------------------------------------------------------------
+testthat::test_that("subsets works", {
   # Works with three numbers
   t1 <- get_subsets(1:3)
   testthat::expect_type(t1, "list")
