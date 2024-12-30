096ec6c044ac3ed5a1b69e3d19c48c1481a41a6a
jmgirard <me@jmgirard.com>
2024-12-30 12:46:48 -0500

    document

--- R/expand.R
+++ R/expand.R
@@ -1,14 +1,16 @@
 #' @export
 expand_optional <- function(required, optional, stringify = TRUE) {
+  # Validate input
+  stopifnot(is.vector(required))
+  stopifnot(is.vector(optional))
+  stopifnot(rlang::is_bool(stringify))
   # Generate all subsets of optional elements
   optional_subsets <- get_subsets(optional)
-  
   # Combine each optional subset with the required elements
   out <- lapply(
     X = optional_subsets, 
     FUN = function(opt_subset) unique(sort(c(required, opt_subset)))
   )
-
   # If requested, stringify the output
   if (stringify) {
     out <- lapply(
@@ -17,7 +19,6 @@ expand_optional <- function(required, optional, stringify = TRUE) {
     )
     out <- unlist(out, recursive = FALSE)
   }
-
   # Return 
   out
 }
@@ -25,8 +26,10 @@ expand_optional <- function(required, optional, stringify = TRUE) {
 get_subsets <- function(vec) {
   # Validate input
   stopifnot(is.vector(vec))
+  # Generate all subsets of vec including empty and full sets
   n <- length(vec)
   if (n == 1) {
+    # Generate manually if n == 1 due to how combn(x) handles single integers
     out <- list(vector(mode = typeof(vec), length = 0), vec)
   } else if (n > 1) {
     out <- lapply(
@@ -37,5 +40,6 @@ get_subsets <- function(vec) {
   } else {
     out <- list(vector(mode = typeof(vec), length = 0))
   }
+  # Return
   out
 }
