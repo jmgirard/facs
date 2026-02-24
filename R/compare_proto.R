5c993b37e406a3071d5e63152b15697d81ff4c07
Jeffrey Girard <me@jmgirard.com>
2026-02-24 09:36:38 -0600

    check x for nonscheme AUs

--- R/compare_proto.R
+++ R/compare_proto.R
@@ -7,9 +7,9 @@
 #'   coded and in which ways (i.e., occurrence, intensity, asymmetry).
 #' @param proto A FUNCTION TO CREATE PROTOTYPES NEEDS TO BE MADE.
 #' @param uncoded A string indicating how to handle the situation where a
-#'   prototype contains an AU that wasn't included in the scheme. "warn"
-#'   provides a warning and an NA value, "error" provides an error and stops,
-#'   and "omit" drops the uncoded AU before calculating the Dice coefficient.
+#'   prototype or coding contains an AU that wasn't included in the scheme.
+#'   "warn" provides a warning and an NA value, "error" provides an error and 
+#'   stops, and "omit" drops the uncoded AU before calculating the Dice coefficient.
 #' @param output A string indicating what to return. "all" returns the Dice
 #'   coefficients for all prototypes, "exact" returns only the prototypes where
 #'   the Dice coefficient equals 1.0, and "closest" returns only the prototypes
@@ -30,9 +30,10 @@ compare_to_prototypes <- function(
   stopifnot(class(scheme) == "facs_scheme")
   uncoded <- match.arg(uncoded, c("warn", "error", "omit"), FALSE)
   output <- match.arg(output, c("all", "exact", "closest"), FALSE)
+  
   # Preallocate output vector
   out <- rep(NA_real_, times = nrow(proto))
-  if (length(unique(proto$source) > 1)) {
+  if (length(unique(proto$source)) > 1) {
     names(out) <- paste(
       proto$source,
       proto$emotion,
@@ -46,44 +47,68 @@ compare_to_prototypes <- function(
       sep = "_"
     )
   }
+  
   # Tidy both sets of codes
   xlist <- occ_list(tidy(x))
   plist <- occ_list(tidy(coding(proto$code)))
-  names(plist) <- paste(
-    proto$source,
-    proto$emotion,
-    proto$config_num,
-    sep = "_"
-  )
-  # Check for prototype codes not in scheme
-  proto_codes <- sort(unique(unlist(plist)))
+  names(plist) <- names(out)
+  
+  # Check for codes not in scheme
   scheme_codes <- scheme$occurrence
-  outside <- setdiff(proto_codes, scheme_codes)
+  
+  proto_codes <- sort(unique(unlist(plist)))
+  outside_proto <- setdiff(proto_codes, scheme_codes)
+  
+  x_codes <- sort(unique(unlist(xlist)))
+  outside_x <- setdiff(x_codes, scheme_codes)
+  
   if (uncoded == "warn") {
-    if (length(outside) > 0) {
+    if (length(outside_proto) > 0) {
       cli::cli_warn(
         paste0(
           "The following codes are present in one or more of the selected ",
           "prototypes but were not included in your coding scheme: [",
-          paste(outside, collapse = ","),
+          paste(outside_proto, collapse = ", "),
+          "]. This will cause some prototype match scores to be NA.\n"
+        )
+      )
+    }
+    if (length(outside_x) > 0) {
+      cli::cli_warn(
+        paste0(
+          "The following codes are present in `x` but were not included ",
+          "in your coding scheme: [",
+          paste(outside_x, collapse = ", "),
           "]. This will cause some prototype match scores to be NA.\n"
         )
       )
     }
   } else if (uncoded == "error") {
-    if (length(outside) > 0) {
+    if (length(outside_proto) > 0) {
       cli::cli_abort(
         paste0(
           "The following codes are present in one or more of the selected ",
           "prototypes but were not included in your coding scheme: [",
-          paste(outside, collapse = ","),
+          paste(outside_proto, collapse = ", "),
+          "].\n"
+        )
+      )
+    }
+    if (length(outside_x) > 0) {
+      cli::cli_abort(
+        paste0(
+          "The following codes are present in `x` but were not included ",
+          "in your coding scheme: [",
+          paste(outside_x, collapse = ", "),
           "].\n"
         )
       )
     }
   } else if (uncoded == "omit") {
-    plist <- lapply(plist, function(x) x[x %in% scheme_codes])
+    plist <- lapply(plist, function(p) p[p %in% scheme_codes])
+    xlist <- lapply(xlist, function(xi) xi[xi %in% scheme_codes])
   }
+  
   # Loop through xlist...
   out <- lapply(xlist, function(xi) {
     # Loop through plist...
@@ -92,12 +117,14 @@ compare_to_prototypes <- function(
       (length(intersect(xi, pi)) * 2) / length(c(xi, pi))
     })
   })
+  
   # Reduce output if requested
   if (output == "exact") {
     out <- lapply(out, function(xi) xi[xi == 1])
   } else if (output == "closest") {
     out <- lapply(out, function(xi) xi[xi == max(xi, na.rm = TRUE)])
   }
+  
   # Return
   out
 }
